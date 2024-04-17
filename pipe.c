bee241bd4cead2dd63bd20be2afde43b64d45a58
Jon Paino <jonpaino@g.ucla.edu>
2024-04-17 07:35:10 -0700

    Successfully handles one command line argument

--- pipe.c
+++ pipe.c
@@ -3,9 +3,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <errno.h>
 
 int main(int argc, char *argv[])
 {
-	
+	// Make the program work with a single command line argument (./pipe ls should output the files in directory)
+	if(argc < 2){
+		errno = EINVAL;
+		perror("Must provide at least one process to run");
+		exit(EINVAL);
+	}
+	execlp(argv[1], argv[1], NULL);
 	return 0;
 }
