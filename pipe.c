567594aa5177f9e461660061bfca3569c9ea3d99
Jon Paino <jonpaino@g.ucla.edu>
2024-04-24 10:22:07 -0700

    The hanging issue was occuring because there was a useless write end to the previous file descriptor.
    Instead I used a single read end to for the previous descriptors and closed all of them accordingly

--- pipe.c
+++ pipe.c
@@ -5,56 +5,63 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-int main(int argc, char *argv[])
-{
-	if(argc < 2){
-		errno = EINVAL;
-		fprintf(stderr, "Usage: %s command1 [command2 ...]\n", argv[0]);
-		exit(EINVAL);
-	}
-	// Create file descriptors for a new a previous pipe for interprocess communication
-	int prev_fd[2];
-	int new_fd[2];
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        errno = EINVAL;
+        fprintf(stderr, "Usage: %s command1 [command2 ...]\n", argv[0]);
+        exit(EINVAL);
+    }
 
-	for(int i = 1; i < argc; i++){
-		// If it is not the last command, create a pipe for the current processes output
-		if (i < argc - 1){
-			pipe(new_fd);
-		}
-		if(fork() == 0){ // Child process
-			// If it is not the first command then read input from previous pipe
-			if (argc > 1){
-				dup2(prev_fd[0], STDIN_FILENO);
-				close(prev_fd[0]);
-			}
-			// If it is not the last command then send output to the new pipe
-			if (i < argc - 1){
-				dup2(new_fd[1], STDOUT_FILENO);
-				close(new_fd[1]);
-			}
-			// Execute the current process
-			execlp(argv[i], argv[i], NULL);
-			perror("execlp failed");
+    int pipe_fd[2];
+    int prev_fd_in = 0;  // Start with stdin being 0 for the first process
+
+    for (int i = 1; i < argc; i++) {
+        if (i < argc - 1) {
+            if (pipe(pipe_fd) == -1) {
+                perror("pipe");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        pid_t pid = fork();
+        if (pid == -1) {
+            perror("fork");
             exit(EXIT_FAILURE);
-		} else {  // Parent process
-			// Close the reading end of the previous pipe if its not the first command
-			if (argc > 1){
-				close(prev_fd[0]);
-			}
-			// Set the previous pipe to the new pipe if its not the last command
-			if (i < argc - 1){
-				prev_fd[0] = new_fd[0];
-				prev_fd[1] = new_fd[1];
-			}
-			wait(NULL);
-		}
-	}
-	int status;
-	while ((wait(&status)) > 0) {
-		if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
-			exit(WEXITSTATUS(status)); // Exit with the error code of the first failing child
-		}
-	}
+        }
 
-	return 0;
+        if (pid == 0) {  // Child process
+            if (prev_fd_in != 0) {
+                dup2(prev_fd_in, 0);  // Use previous input for stdin
+                close(prev_fd_in);   // Close original fd
+            }
+
+            if (i < argc - 1) {
+                dup2(pipe_fd[1], 1);  // Direct stdout to the write-end of the pipe
+                close(pipe_fd[0]);   // Close read-end in the child
+                close(pipe_fd[1]);   // Close original fd
+            }
+
+            execlp(argv[i], argv[i], NULL);
+            perror("execlp");
+            exit(EXIT_FAILURE);
+        }
+
+        // Parent process
+        if (prev_fd_in != 0) {
+            close(prev_fd_in);  // Close previous input
+        }
+
+        if (i < argc - 1) {
+            prev_fd_in = pipe_fd[0];  // Store the read-end for the next iteration
+            close(pipe_fd[1]);       // Close write-end in the parent
+        }
+
+		int status;
+        wait(&status);  // Wait for the child process to finish
+		if (WIFEXITED(status) && WEXITSTATUS(status) != 0) {
+            exit(WEXITSTATUS(status));  // Exit with non-zero status if any child failed
+        }
+    }
+
+    return 0;
 }
